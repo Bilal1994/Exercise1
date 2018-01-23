9800f5457d442b4a86f4b40f5cc8d5256696a39f
Bilal1994 <bilal.ahmed@tum.de>
2018-01-23 17:40:30 +0100

    Done

---
+++ src/HelloUser.java
@@ -0,0 +1,15 @@
+
+public class HelloUser {
+String userName;
+
+HelloUser(String uN)
+{
+	this.userName=uN;
+}
+
+void greetUser()
+{
+	System.out.println("Hello "+this.userName);
+	
+}
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello ASE2017 - How are you? :)");
+		System.out.println("Please enter your name: ");
+		Scanner reader = new Scanner(System.in);  // Reading from System.in
+		  String n = reader.nextLine();		reader.close(); 
+		HelloUser hU = new HelloUser(n);
+		hU.greetUser();
 	}
 
 }
