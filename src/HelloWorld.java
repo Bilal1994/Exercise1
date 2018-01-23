fb37a815c2df845ec9cce29ca0f4212bdb9b28a3
Bilal1994 <bilal.ahmed@tum.de>
2018-01-23 17:52:05 +0100

    Modified - 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,12 +4,12 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello ASE2017 - How are you? :)");
+		System.out.println("Hello ASE2017 - How are you now? :)");
 		System.out.println("Please enter your name: ");
 		Scanner reader = new Scanner(System.in);  // Reading from System.in
 		  String n = reader.nextLine();		reader.close(); 
-		HelloUser hU = new HelloUser(n);
-		hU.greetUser();
+		// HelloUser hU = new HelloUser(n);
+		// hU.greetUser();
 	}
 
 }
